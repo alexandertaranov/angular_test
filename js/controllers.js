0ad692c1c1ae9c7b942f928335c063b277c7e3d9
Alexander Taranov <sander.taranov@gmail.com>
2015-01-17 23:52:53 +0600

    new_prj

--- js/controllers.js
+++ js/controllers.js
@@ -28,3 +28,27 @@ phonecatApp.controller('PhoneListCtrl', function($scope) {
     }
 
 });
+
+var dataTable = angular.module('dataTable', []);
+dataTable.controller('NewsListController', function($scope){
+    $scope.news = [
+        {'number': "1",
+            'text': "Apache2",
+            'date': "23.01.18"},
+        {'number': "2",
+            'text': "Virtual Box",
+            'date': "21.01.14"},
+        {'number': "3",
+            'text': "Bodog",
+            'date': "25.01.14"}
+    ];
+    $scope.filterName = undefined;
+    $scope.sort = function(filterValue) {
+        if ($scope.filterName != filterValue){
+            $scope.filterName = filterValue;
+        }
+        else {
+            $scope.filterName = undefined;
+        }
+    }
+});
